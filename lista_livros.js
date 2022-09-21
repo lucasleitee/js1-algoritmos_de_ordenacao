955ae9cd5459256c589452521ad9b61d88397d80
lucasleitee <lukas_vit@hotmail.com>
2022-09-21 15:29:27 -0300

    aula 1

---
+++ lista_livros.js
@@ -0,0 +1,28 @@
+const precosLivros = [
+{
+    titulo:"JavaScript", 
+    preco:25
+},
+{
+   titulo:"PHP",
+    preco:15
+},
+{
+    titulo:"Java",
+    preco:30
+},
+{
+    titulo:"Elixir",
+    preco:50
+},
+{
+    titulo:"",
+    preco: 45
+},
+{
+    titulo:"",
+    preco: 20
+}
+];
+
+module.exports = precosLivros;
